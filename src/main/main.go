3c19dca2aa402a01f26b169731d28e081c2fd4aa
Chixm <makoto0072104@yahoo.co.jp>
2019-01-04 16:19:54 +0900

    Add Comments

--- src/main/main.go
+++ src/main/main.go
@@ -18,15 +18,18 @@ func main() {
 	log.Println(`End Main`)
 }
 
+// This method represents task that is able to cancel.
 func LongTermTask(ctx context.Context) error {
 	log.Println(`Long Term Task`)
 
 	rand.Seed(time.Now().UnixNano())
 
+	// Intended Error Occurs to cancel the LongTermTask
 	if c := rand.Intn(200); c < 100 {
 		return errors.New(`Something happened. >> ` + strconv.Itoa(c))
 	}
 
+	// Pretending Task working
 	time.Sleep(10 * time.Second)
 
 	return nil
